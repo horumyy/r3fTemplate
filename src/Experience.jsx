0e64dc4e32e66b33da0d7168ecbebf411b80c3b5
Jorge Plasencia <jorgeplasenciaaj@gmail.com>
2024-01-31 18:11:45 -0700

    added shaders folder template and path alias

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,9 +1,12 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { useControls } from "leva";
+import basicVertexShader from "@/assets/shaders/basic.vert";
 
 export default function Experience() {
   const { color } = useControls({ color: "#e3bfd4" });
+
+  console.log(basicVertexShader);
 
   return (
     <Canvas>
